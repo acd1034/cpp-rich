59a2670c087352dc2542f06f32db908c98349a6c
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-08-15 15:41:41 +0900

    ✨ Add format_spec

---
+++ include/rich/style/format_spec.hpp
@@ -0,0 +1,47 @@
+/// @file format_spec.hpp
+#pragma once
+#include <rich/format.hpp>
+
+namespace rich {
+  template <typename Char>
+  struct format_spec {
+    std::basic_string_view<Char> fill{};
+    Char align{};
+    std::size_t width{};
+    fmt::text_style style{};
+  };
+
+  /* template <typename Char>
+  auto fill(format_spec<Char>, const Char*);
+
+  template <>
+  auto fill(format_spec<char> fs, const char* newfill) {
+    fs.fill = std::string_view(newfill);
+    return fs;
+  } */
+
+  template <typename Char>
+  auto invert(format_spec<Char>);
+
+  template <>
+  auto invert(format_spec<char> fs) {
+    switch (fs.align) {
+    case '<':
+      fs.align = '>';
+      break;
+    case '>':
+      fs.align = '<';
+      break;
+    }
+    return fs;
+  }
+
+  template <class Out, typename Char>
+  requires ranges::output_iterator<Out, const Char&>
+    Out _format_to(Out out, const format_spec<Char>& fs, const Char* s) {
+    const auto fmtstr =
+      fmt::format("{{:{0}{1}{2}}}", fs.fill, fs.align, fs.width);
+    return fmt::vformat_to(out, fs.style, fmt::detail::to_string_view(fmtstr),
+                           fmt::make_format_args(s));
+  }
+} // namespace rich
