c178ebb4d176f43471aec0099d10b32d1bbbe8ca
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-08-15 15:42:08 +0900

    ✨ Add params for boarder of panel

--- include/rich/style/panel.hpp
+++ include/rich/style/panel.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 
 #include <rich/format.hpp>
+#include <rich/style/format_spec.hpp>
 #include <rich/style/line_formatter.hpp>
 #include <rich/style/lines.hpp>
 #include <rich/style/segment.hpp>
@@ -12,19 +13,23 @@ namespace rich {
   // https://github.com/Textualize/rich/blob/5d3f600f43796393a2c3e4cb20d807c5cf147f44/rich/panel.py#L38-L53
   template <class L>
   struct panel {
+    using char_type = typename L::char_type;
     L lines{};
-    fmt::text_style style{};
-    std::size_t width = 60;
+    format_spec<char_type> boarder_spec{
+      .fill = " ",
+      .align = '<',
+      .width = 2,
+      .style = fg(fmt::terminal_color::red),
+    };
+    std::size_t width = 80;
 
     panel() = default;
-    constexpr explicit panel(const L& l, fmt::text_style s = {})
-      : lines(l), style(s) {}
-    constexpr explicit panel(L&& l, fmt::text_style s = {})
-      : lines(std::move(l)), style(s) {}
+    constexpr explicit panel(const L& l, int = {}) : lines(l) {}
+    constexpr explicit panel(L&& l, int = {}) : lines(std::move(l)) {}
   };
 
   template <line_range R>
-  panel(R&&, fmt::text_style = {})
+  panel(R&&, int = {})
     -> panel<lines<typename ranges::range_value_t<R>::char_type>>;
 } // namespace rich
 
@@ -50,20 +55,35 @@ public:
     const auto w = std::min(ptr_->width, n);
     assert(w != line_formatter_npos);
     switch (phase_) {
-    case 0:
+    case 0: {
       ++phase_;
-      return {fmt::format_to(out, ptr_->style, "╭{0:─<{1}}╮", "", w - 2), w};
+      auto boarder_spec = ptr_->boarder_spec;
+      if (ptr_->boarder_spec.align == '<')
+        boarder_spec.fill = std::string_view("─");
+      out = _format_to(out, boarder_spec, "╭");
+      out = fmt::format_to(out, boarder_spec.style, "{0:─<{1}}", "",
+                           w - boarder_spec.width * 2);
+      return {_format_to(out, invert(boarder_spec), "╮"), w};
+    }
     case 1: {
       if (!line_fmtr_) {
         ++phase_;
-        return {fmt::format_to(out, ptr_->style, "╰{0:─<{1}}╯", "", w - 2), w};
+        auto boarder_spec = ptr_->boarder_spec;
+        if (ptr_->boarder_spec.align == '<')
+          boarder_spec.fill = std::string_view("─");
+        out = _format_to(out, boarder_spec, "╰");
+        out = fmt::format_to(out, boarder_spec.style, "{0:─<{1}}", "",
+                             w - boarder_spec.width * 2);
+        return {_format_to(out, invert(boarder_spec), "╯"), w};
       }
-      out = fmt::format_to(out, ptr_->style, "│ ");
-      auto result = line_fmtr_.format_to(out, w - 4);
+
+      out = _format_to(out, ptr_->boarder_spec, "│");
+      auto w2 = w - ptr_->boarder_spec.width * 2;
+      auto result = line_fmtr_.format_to(out, w2);
       out = result.out;
-      if (result.size < w - 4)
-        fmt::format_to(out, "{0: <{1}}", "", w - 4 - result.size);
-      return {fmt::format_to(out, ptr_->style, " │"), w};
+      if (result.size < w2)
+        out = fmt::format_to(out, "{0: <{1}}", "", w2 - result.size);
+      return {_format_to(out, invert(ptr_->boarder_spec), "│"), w};
     }
     default:
       RICH_UNREACHABLE();
