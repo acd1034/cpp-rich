3b305ba7d75fd626799237178d6be709b23d1258
acd1034 <48613285+acd1034@users.noreply.github.com>
2022-08-16 22:57:45 +0900

    ⚡️ Use spec_format_to, rspec_format_to

--- include/rich/style/format_spec.hpp
+++ include/rich/style/format_spec.hpp
@@ -1,6 +1,7 @@
 /// @file format_spec.hpp
 #pragma once
 #include <rich/format.hpp>
+#include <rich/saturation.hpp>
 
 namespace rich {
   template <typename Char>
@@ -24,9 +25,19 @@ namespace rich {
     return fs;
   }
 
+  // `sv` must contain only zero or one character
   template <typename Char, ranges::output_iterator<const Char&> Out>
   Out spec_format_to(Out out, const format_spec<Char>& fs,
                      std::basic_string_view<Char> sv) {
-    return aligned_format_to(out, fs.style, sv, fs.fill, fs.align, fs.width);
+    auto width = sv.empty() ? fs.width : sat_sub(fs.width, 1);
+    return aligned_format_to(out, fs.style, sv, fs.fill, fs.align, width);
+  }
+
+  // `sv` must contain only zero or one character
+  template <typename Char, ranges::output_iterator<const Char&> Out>
+  Out rspec_format_to(Out out, const format_spec<Char>& fs,
+                      std::basic_string_view<Char> sv) {
+    auto width = sv.empty() ? fs.width : sat_sub(fs.width, 1);
+    return reversed_format_to(out, fs.style, sv, fs.fill, fs.align, width);
   }
 } // namespace rich
--- include/rich/style/panel.hpp
+++ include/rich/style/panel.hpp
@@ -66,49 +66,44 @@ public:
     -> fmt::format_to_n_result<Out> {
     assert(ptr_ != nullptr);
     const auto width = std::min(ptr_->contents_spec.width, n);
-    const auto& bs = ptr_->boarder_spec;
-    assert(bs.width * 2 < width and width < line_formatter_npos);
+    assert(ptr_->boarder_spec.width * 2 < width
+           and width < line_formatter_npos);
 
     switch (phase_) {
     case 0: {
       ++phase_;
+      auto bs = ptr_->boarder_spec;
       using enum align_t;
+      if (bs.align == left)
+        bs.fill = std::string_view("─");
       // clang-format off
-      if (bs.align == left)
-        out = aligned_format_to<Char>(out, bs.style, "╭", "─", bs.align, bs.width - 1);
-      else
-        out = aligned_format_to<Char>(out, bs.style, "╭", bs.fill, bs.align, bs.width - 1);
+      out = spec_format_to<Char>(out, bs, "╭");
       out = aligned_format_to<Char>(out, bs.style, ptr_->title, "─", center, npos_sub(width, bs.width * 2 + ptr_->title.size()));
-      if (bs.align == left)
-        out = reversed_format_to<Char>(out, bs.style, "╮", "─", bs.align, bs.width - 1);
-      else
-        out = reversed_format_to<Char>(out, bs.style, "╮", bs.fill, bs.align, bs.width - 1);
+      out = rspec_format_to<Char>(out, bs, "╮");
       // clang-format on
       return {out, width};
     }
     case 1: {
       if (!line_fmtr_) {
         ++phase_;
+        auto bs = ptr_->boarder_spec;
         using enum align_t;
+        if (bs.align == left)
+          bs.fill = std::string_view("─");
         // clang-format off
-        if (bs.align == left)
-          out = aligned_format_to<Char>(out, bs.style, "╰", "─", bs.align, bs.width - 1);
-        else
-          out = aligned_format_to<Char>(out, bs.style, "╰", bs.fill, bs.align, bs.width - 1);
+        out = spec_format_to<Char>(out, bs, "╰");
         out = aligned_format_to<Char>(out, bs.style, "", "─", {}, npos_sub(width, bs.width * 2));
-        if (bs.align == left)
-          out = reversed_format_to<Char>(out, bs.style, "╯", "─", bs.align, bs.width - 1);
-        else
-          out = reversed_format_to<Char>(out, bs.style, "╯", bs.fill, bs.align, bs.width - 1);
+        out = rspec_format_to<Char>(out, bs, "╯");
         // clang-format on
         return {out, width};
       }
 
       const auto& cs = ptr_->contents_spec;
+      const auto& bs = ptr_->boarder_spec;
       // clang-format off
-      out = aligned_format_to<Char>(out, bs.style, "│", bs.fill, bs.align, bs.width - 1);
+      out = spec_format_to<Char>(out, bs, "│");
       out = line_format_to<Char>(out, cs.style, line_fmtr_, cs.fill, cs.align, npos_sub(width, bs.width * 2));
-      out = reversed_format_to<Char>(out, bs.style, "│", bs.fill, bs.align, bs.width - 1);
+      out = rspec_format_to<Char>(out, bs, "│");
       // clang-format on
       return {out, width};
     }
